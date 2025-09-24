30cf6aaa59ea11cf78558c18199e3f35482e9eb8
mwgabriele <gw06mayer@gmail.com>
2025-09-24 13:28:22 +0000

    primeira página js para teste

---
+++ pagina.js
@@ -0,0 +1,6 @@
+document.body.innerHTML = "<h1>Olá, Mundo!</h1>"
+cabecalho = document.createElement("h1")
+cabecalho.innerText = "Cabeçalho JS"
+document.body.appendChild(cabecalho)
+txNome = document.createElement("input")
+document.body.appendChild(txNome)
